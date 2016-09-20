88a02f442224c0c98d6aa324f0df97dd0a7e02b9
Saba El-Hilo <saba@signalsciences.com>
2016-09-20 12:31:40 -0700

    More method examples

--- methods/methods.go
+++ methods/methods.go
@@ -1,1 +1,30 @@
 package methods
+
+import (
+	"math"
+	"fmt"
+)
+
+type Face interface {
+	Abs() float64
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64  {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *Vertex) EmptyInterfaceExample() {
+	var emptyI interface{} = "string"
+	value, ok := emptyI.(string)
+	fmt.Println("value and ok: ", value, ok)
+}
+
